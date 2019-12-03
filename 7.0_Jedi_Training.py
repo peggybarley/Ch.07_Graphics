beaff36d69ea8f5d56c692fa09e4d318c7df10ed
barleypeg <barleypeg@urbandaleschools.com>
2019-12-03 14:24:00 -0600

    ch. 7

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -6,4 +6,3 @@ BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
 
-import arcade
